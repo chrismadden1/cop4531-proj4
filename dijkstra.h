004ee0e5f34a41f15db84f1cc4fe1d4b4a16b741
chrismadden1 <46973709+chrismadden1@users.noreply.github.com>
2019-07-09 09:45:06 -0400

    Create dijkstra.h

---
+++ dijkstra.h
@@ -0,0 +1,34 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+template < class G>
+class Dijkstra
+{
+  typedef G                      Graph
+  typedef typename G::Vertex     Vertex;
+  ... // more definitions optional
+
+public:
+  // constructor initializes all class variables in init list
+  Dijkstra (const G& g);
+
+  // implementing the algorithm
+  void Init(Vertex source); // preps variables for startup of algorithm
+  void Exec();              // executes algorithm
+
+  // extracting information
+  const fsu::Vector<double>& Distance() const;
+  const fsu::Vector<size_t>& Parent() const;
+  void                       Path(Vertex x, fsu::List<Vertex>& path) const;
+  ... // expand API optional
+
+private: // data
+  const Graph& g_;
+  ...
+
+private: // methods
+  void Relax(Vertex v)
+  ...
+};
+
+#endif /* DIJKSTRA_H */
