51b57b6f5fd31b623c26d4c87c5df1f4cf1ee263
chrismadden1 <46973709+chrismadden1@users.noreply.github.com>
2019-07-14 18:47:07 -0400

    Add files via upload

--- wgraph.h
+++ wgraph.h
@@ -1,185 +1,268 @@
 /*
     wgraph.h
-    7/08/2019
     Chris Madden
-    implements weighted graph class using unweighted graphs
+    07/10/19
+
 */
 
-#ifndef WGRAPH_H
-#define WGRAPH_H
+#ifndef _WGRAPH_H
+#define _WGRAPH_H
 
-#include <graph_util.h>
-#include <list.h>
+#include <iostream>
 #include <vector.h>
-#include <cstdlib>
+// #include <ovector.h>
+#include <list.h>
+// #include <olist.h>
+// #include <rbllt_adt.h>
+// #include <matrix.h>
+ #include "graph.h"
+ #include "aa.h"
+ #include "hashclasses.h"
+ #include <xstring.cpp>
 
-namespace fsu {
-    
-    template < typename N >
-    class ALUWGraph : public ALUGraph <N>
-    {
-    public:
-        
-        typedef N                                   	 Vertex;
-	//typedef typename fsu::List<Vertex>               SetType;
-	//****need to map an edge to a real number value.
-	//add two edges together like key = ToHex(x)+"."+ToHex(y). 
-	typedef typename HashTable<fsu::String, double>  SetType;
-        typedef typename SetType::ConstIterator    	 AdjIterator;
-        
-        void     SetVrtxSize (N n);
-        size_t   VrtxSize    () const;
-        void     AddEdge     (Vertex from, Vertex to, double weight = 1.0);
-	bool     HasEdge     (Vertex from, Vertex to, double& wt) const; // sets wt variable if edge exists
-	//Edge<N>  GetEdge     (Vertex from, Vertex to) const;             // returns [0,0,0.0] if edge does not exist
-	void     SetWeight   (Vertex from, Vertex to, double weight);    // (re)sets weight
-	double   GetWeight   (Vertex from, Vertex to) const;             // returns weight if edge exists, 0.0 otherwise
-        size_t   EdgeSize    () const;
-        size_t   OutDegree   (Vertex v) const;  
-        size_t   InDegree    (Vertex v) const;    
-        
-        AdjIterator Begin   (Vertex x) const;
-        AdjIterator End     (Vertex x) const;
-        
-        ALUWGraph            (); //constructor
-        explicit ALUWGraph   (N n); //constructor with no type converting allowed
-        
-    protected:
-	fsu::Vector <SetType> al_; //for mapping edges to weight.
-        
-    }; //end class ALUWGraph
-    
-    
-    //Directed graph - derived from undirected graph
-    template < typename N >
-    class ALDWGraph : public ALDGraph <N>
-    {
-    public:
-        typedef N                                    Vertex;
-        typedef typename ALUWGraph<N>::SetType       SetType;
-        typedef typename ALUWGraph<N>::AdjIterator   AdjIterator;
-        
-        void    AddEdge    (Vertex from, Vertex to, double weight = 1.0);
-        size_t  EdgeSize   () const;
-        size_t  InDegree   (Vertex v) const;
-        void 	Reverse    (ALDWGraph & d) const;
-        
-        ALDWGraph            ();
-        explicit ALDWGraph   (N n);
-        
-    }; //end class ALDWGraph
-    
-    template < typename N >
-    ALUWGraph<N>::ALUWGraph() : al_(0){} 
-    
-    template < typename N >
-    ALUWGraph<N>::ALUWGraph(N n) : al_((size_t)n){}	
+namespace fsu
+{
 
-    template < typename N >
-    ALUWGraph<N>::SetVrtxSize(N n)
-    {
-	    al_.Rehash(n);
-    }
-	
+  template < typename N >
+  class ALUWGraph : public ALUGraph <N>
+  {
+  public:
+    typedef N                                      Vertex;
+    typedef fsu::String                            KeyType;
+    typedef double                                 DataType;
+    typedef hashclass::KISS < KeyType >            HashType;
+    typedef fsu::Entry < KeyType, DataType >       EntryType;
+    typedef fsu::HashTable<KeyType, DataType, HashType > SetType;
+    typedef typename SetType::ConstIterator        AdjIterator;
 
-    template < typename N >
-    void ALUWGraph<N>::AddEdge(Vertex from, Vertex to, double wt)
-    {
-        al_[from].Insert(to); //adds "to" vertex to the corresponding list
-        al_[to].Insert(from);
-	//al_ = {(from, to)->wt};
-	fsu::String key = ToHex(from) + "." + ToHex(to);
-	al_.Insert(key, wt);
-    }
-	
-    template < typename N >
-    bool ALUWGraph<N>::HasEdge (Vertex from, Vertex to, double &wt) const
-    {
-        AdjIterator i = al_[from].Includes(to);
-        if (i == al_[from].End())
-            return 0; //not found
-	fsu::String key = ToHex(from) + "." + ToHex(to);
-	i.Insert(key, wt);
-        return 1; //found
-    }
-	
-	
-	template < typename N >
-	size_t ALUWGraph<N>::EdgeSize () const // Theta (|V| + |E|)
-	{
-	size_t esize = 0;
-	for (Vertex v = 0; v < al_.Size(); ++v)
-		esize += al_[v].Size();
-	return esize >> 1;
-	}
-	
-	template < typename N >
-    void ALUWGraph<N>::SetWeight(Vertex from, Vertex to, double wt)
-    {
-       fsu::String key = ToHex(from) + "." + ToHex(to);
-       al_.Insert(key, wt);
-    }
-	
-	template < typename N >
-    void ALUWGraph<N>::OutDegree(Vertex v)
-    {
-      return al_[v].Size(); //return size of list corresponding to vertex
-    }
-	
-	template < typename N >
-    void ALUWGraph<N>::InDegree(Vertex v)
-    {
-      return OutDegree(v); //return size of list corresponding to vertex
-    }
-	
-	template < typename N >
-    typename ALUWGraph<N>::AdjIterator ALUWGraph<N>::Begin (Vertex x) const
-    {
-        return al_[x].Begin(); //returns Begin list iterator
-    }
-    
-    template < typename N >
-    typename ALUWGraph<N>::AdjIterator ALUWGraph<N>::End (Vertex x) const
-    {
-        return al_[x].End(); //returns End list iterator
-    }
+    void   SetVrtxSize  (N n);
+    size_t VrtxSize     () const;
+    void   AddEdge      (Vertex from, Vertex to, double wt = 1.0);
+    bool   HasEdge      (Vertex from, Vertex to) const;
+    size_t EdgeSize     () const;           // Theta (|V| + |E|)
+    size_t OutDegree    (Vertex v) const;
+    size_t InDegree     (Vertex v) const;
+    void   SetWeight  (Vertex from, Vertex to, double weight);
+    void   GetWeight  (Vertex from, Vertex to);
 
-    template < typename N >
-    ALDGraph<N>::ALDGraph () : ALUGraph<N>(){} //default constructor
-    
-    template < typename N >
-    ALDGraph<N>::ALDGraph(N n) : ALUGraph<N> (n){}
-	
-    template < typename N >
-    void ALDWGraph<N>::AddEdge(Vertex from, Vertex to, double weight)
-    {
-      (this->al_)[from].Insert(to);
-	fsu::String key = ToHex(from) + "." + ToHex(to);
-	al_.Put(key, wt);
-    }
-	
-    template < typename N >
-    void ALDWGraph<N>::EdgeSize()
-    {
-        size_t esize = 0;
-        for (Vertex v = 0; v < (this->al_).Size(); ++v)
-            esize += ALUWGraph<N>::al_[v].Size();
-        return esize;
-    }
-	
-    template < typename N >
-    void ALDWGraph<N>::InDegree(Vertex v) const
-    {
-      size_t indegree = 0;
-        AdjIterator j;
-        for (Vertex x = 0; x < this->VrtxSize(); ++x)
-        {
-            for (j = this->Begin(x); j != this->End(x); ++j)
-            {
-                if (v == *j) ++indegree;
-            }
-        }
-        return indegree;
-    }	
+    AdjIterator Begin   (Vertex v) const;
+    AdjIterator End     (Vertex v) const;
 
+    void Clear          ();
+    void Dump           (std::ostream& os); // Theta (|V| + |E|)
+    void Shuffle        (); // shuffles all adjacency lists
+
+    ALUWGraph            ();
+    explicit ALUWGraph   (N n);
+
+  protected:
+    SetType al_;
+  };
+
+  template < typename N >
+  class ALDWGraph : public ALDGraph <N>
+  {
+  public:
+    typedef N                                          Vertex;
+    typedef fsu::String                            KeyType;
+    typedef double                                 DataType;
+    typedef hashclass::KISS < KeyType >            HashType;
+    typedef fsu::Entry < KeyType, DataType >       EntryType;
+    typedef fsu::HashTable<KeyType, DataType, HashType > SetType;
+    typedef typename ALUGraph<N>::AdjIterator          AdjIterator;
+
+     void   SetVrtxSize  (N n);
+    // size_t VrtxSize     () const;
+    void   AddEdge      (Vertex from, Vertex to, double wt = 1.0);
+    // bool   HasEdge      (Vertex from, Vertex to) const;
+    size_t EdgeSize     () const;          // Theta (|V| + |E|)
+    // size_t OutDegree    (Vertex v) const;
+    size_t InDegree     (Vertex v) const;  // Theta (|V| + |E|)
+
+    ALDWGraph ( );
+    explicit ALDWGraph ( N n );
+  protected:
+    SetType al_;
+  //  Vector < SetType >  al_;
+    // new method - creates d as the reverse directed graph of *this
+  };
+
+  /////////////////////////
+  // ALU implementations //
+  /////////////////////////
+
+  template < typename N >
+  ALUWGraph<N>::ALUWGraph () : al_(0){}
+
+  template < typename N >
+  ALUWGraph<N>::ALUWGraph ( N n ) : al_((size_t)n){}
+
+  template < typename N >
+  void ALUWGraph<N>::SetVrtxSize (N n)
+  {
+    al_.Rehash((size_t)n);
+  }
+
+  template < typename N >
+  void ALUWGraph<N>::AddEdge (Vertex from, Vertex to, double wt)
+  {
+    if (from == to) return;
+  //  al_.Insert(to);
+  //  al_[(size_t)to].Insert(from);
+    fsu::String key = ToHex(from) + "." + ToHex(to);
+	  al_.Insert(key, wt);
+  }
+
+  template < typename N >
+  bool ALUWGraph<N>::HasEdge (Vertex from, Vertex to) const
+  {
+    fsu::String key = ToHex(from) + "." + ToHex(to);
+    AdjIterator i = al_.Includes(key);
+    if (i == End(from))
+      return 0;
+    return 1;
+  }
+
+  template < typename N >
+  size_t ALUWGraph<N>::VrtxSize () const
+  {
+    return al_.Size();
+  }
+
+  template < typename N >
+  size_t ALUWGraph<N>::EdgeSize () const // Theta (|V| + |E|)
+  // Theta (|V| + |E|)
+  {
+    size_t esize = 0;
+    for (Vertex v = 0; v < al_.Size(); ++v)
+      esize += al_.Size();
+    return esize >> 1; // divide by 2
+  }
+
+  template < typename N >
+  size_t ALUWGraph<N>::OutDegree (Vertex v) const
+  {
+    return al_[v].Size();
+  }
+
+  template < typename N >
+  size_t ALUWGraph<N>::InDegree (Vertex v) const
+  {
+    return OutDegree(v);
+  }
+
+  template < typename N >
+  typename ALUWGraph<N>::AdjIterator ALUWGraph<N>::Begin (Vertex v) const
+  {
+    AdjIterator i = al_.Begin();
+    return i;
+  }
+
+  template < typename N >
+  typename ALUWGraph<N>::AdjIterator ALUWGraph<N>::End (Vertex v) const
+  {
+    AdjIterator i = al_.Begin();
+    return i;
+  }
+
+  template < typename N >
+  void ALUWGraph<N>::Dump (std::ostream& os)
+  // Theta (|V| + |E|)
+  {
+    AdjIterator i;
+    for (Vertex v = 0; v < VrtxSize(); ++v)
+    {
+      os << '[' << v << "]->";
+      i = this->Begin(v);
+      if (i != this->End(v))
+      {
+	os << *i;
+	++i;
+      }
+      for ( ; i != this->End(v); ++i)
+      {
+	os << ',' << *i;
+      }
+      os << '\n';
+    }
+  }
+
+  template < typename N >
+  void ALUWGraph<N>::Shuffle()
+  {
+    for (Vertex v = 0; v < VrtxSize(); ++v) al_[v].Shuffle();
+  }
+
+  template < typename N >
+  void ALUWGraph<N>::Clear()
+  {
+    for (Vertex v = 0; v < VrtxSize(); ++v) al_[v].Clear();
+  }
+
+  /////////////////////////
+  // ALD implementations //
+  /////////////////////////
+
+  template < typename N >
+  ALDWGraph<N>::ALDWGraph () : ALDGraph<N> (){}
+
+  template < typename N >
+  ALDWGraph<N>::ALDWGraph ( N n ) : ALDGraph<N>(n){}
+
+  template < typename N >
+  void ALDWGraph<N>::SetVrtxSize (N n)
+  {
+    al_.Rehash((size_t)n);
+  }
+  template < typename N >
+  void ALDWGraph<N>::AddEdge (Vertex from, Vertex to, double wt)
+  {
+    if (from == to) return;
+  //  ALUWGraph<N>::al_[(size_t)from].Insert(to);
+    fsu::String key = ToHex(from) + "." + ToHex(to);
+	  al_.Insert(key, wt);
+  }
+
+  template < typename N >
+  size_t ALDWGraph<N>::EdgeSize () const
+  // Theta (|V| + |E|)
+  {
+    size_t esize = 0;
+    for (Vertex v = 0; v < ALUGraph<N>::al_.Size(); ++v)
+      esize += ALUGraph<N>::al_[v].Size();
+    return esize;
+  }
+
+  template < typename N >
+  size_t ALDWGraph<N>::InDegree (Vertex v) const
+  // Theta (|V| + |E|)
+  {
+    size_t indegree = 0;
+    AdjIterator i;
+    for (Vertex x = 0; x < ALUGraph<N>::VrtxSize(); ++x)
+    {
+      for (i = this->Begin(x); i != this->End(x); ++i)
+      {
+	if (v == *i) ++indegree;
+      }
+    }
+    return indegree;
+  }
+
+/*  template < typename N >
+  void ALDWGraph<N>::Reverse(ALDGraph& d) const
+  {
+    d.Clear();
+    d.SetVrtxSize(this->VrtxSize());
+    for (Vertex v = 0; v < this->VrtxSize(); ++v)
+    {
+      for (AdjIterator i = this->Begin(v); i != this->End(v); ++i)
+      {
+        d.AddEdge(*i,v);
+      }
+    }
+    return;
+  }*/
+
+} // namespace fsu
 #endif
