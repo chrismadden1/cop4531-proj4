d580c53d6f3a322343ab5c9b662cf61ec0e660a4
chrismadden1 <46973709+chrismadden1@users.noreply.github.com>
2019-07-16 06:38:30 -0400

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -11,56 +11,61 @@
 template < class G>
 class Dijkstra
 {
-  typedef G                      Graph
-  typedef typename G::Vertex     Vertex;
-
+  typedef G                                          Graph;
+  typedef typename G::Vertex                         Vertex;
+  typedef fsu::Pair<double,Vertex>                   DV;
+  typedef fsu::Vector<DV>                            Container;
+  typedef fsu::GreaterThan<DV>                       Predicate;
+  typedef fsu::PriorityQueue<DV,Container,Predicate> PQ;
 public:
   // constructor initializes all class variables in init list
-  Dijkstra (const G& g);
+  Dijkstra (const G& g) : g_(g),start_(0),c_(0),pred_(),pq_(c_,pred_),d_(0), p_(0), hasNegativeWeight_(0) {}
 
   // implementing the algorithm
   void Init(Vertex source); // preps variables for startup of algorithm
   void Exec();              // executes algorithm
 
   // extracting information
-  const fsu::Vector<double>& Distance() const;
-  const fsu::Vector<size_t>& Parent() const;
-  void                       Path(Vertex x, fsu::List<Vertex>& path) const;
+  //const fsu::Vector<double>& Distance() const;
+  //const fsu::Vector<size_t>& Parent() const;
+  //void                       Path(Vertex x, fsu::List<Vertex>& path) const;
 
 private: // data
-  const Graph& g_;
+  void Relax(Vertex v);
+  const G&            g_;    // <- directed weighted graph
+  Vertex              start_;
+  Container           c_;    // <- "input" edge set
+  Predicate           pred_; // vertex prioritizer
+  PQ                  pq_;   // priority queue package
+  fsu::Vector<double> d_; // distance
+  fsu::Vector<Vertex> p_; // predecessor in search
+  bool                hasNegativeWeight_;
 
 private: // methods
-  void Relax(Vertex v)
+  void Relax(Vertex x, Vertex y, double wt, PQ);
 };
 
 template < class G>
-void Dijkstra<G>::Init(vertex source)
+void Dijkstra<G>::Init(Vertex source)
 {
  /*
- Initialize-single-source (G,s);
-Initialize finished to false
-Initialize Q = V with priority d
+ sort the priority queue pq_
  */
     
 }
 
 template < class G>
-void Dijkstra<G>::Exec(vertex source)
+void Dijkstra<G>::Exec(Vertex source)
 {
- /*
- while (!Q.Empty())
+ pq_.Push(source);
+ while (!pq_.Empty())
 {
-x = Q.Front();
-Q.Pop();
-if (finished[x]) continue;
-for each vertex y adjacent from x
-{
-Relax2(x,y,w,Q);
+Vertex u = pq_.Front();
+pq_.Pop();
+//for(each vertex y adjacent to x)
+        Relax3(x,y,w,pq_);
 }
-finished[x] = true;
-}
- */
+ 
     
 }
 #endif /* DIJKSTRA_H */
