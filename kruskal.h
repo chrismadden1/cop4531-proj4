41ef0580e4448f9db2a68b01c91b25edb3ae39ed
chrismadden1 <46973709+chrismadden1@users.noreply.github.com>
2019-07-30 12:32:43 -0400

    Update kruskal.h

--- kruskal.h
+++ kruskal.h
@@ -8,66 +8,32 @@
 #ifndef KRUSKAL_H
 #define KRUSKAL_H
 
-template < class G>
-class Kruskal 
+template < class G >
+Kruskal
 {
-  typedef G                      Graph;
-  typedef typename G::Vertex     Vertex;
-  //typedef fsu::Edge<Vertex>      Edge;
- // typedef fsu::Vector<Edge>      Container;
-  //typedef fsu::GreaterThan<Edge> Predicate;
- // typedef fsu::PriorityQueue<Edge,Container,Predicate> PQ;
-
+  typedef G             Graph;
+  typedef G::Vertex     Vertex;
+  typedef fsu::Edge<Vertex>                            Edge;
+  typedef fsu::Vector<Edge>                            Container;
+  typedef fsu::GreaterThan<Edge>                       Predicate;
+  typedef fsu::PriorityQueue<Edge,Container,Predicate> PQ;
 
 public:
-  // constructor initializes all class variables in init list
-  Kruskal (const G& g);
+  void              Init   ( bool verbose = 0 );
+  void              Exec   ( bool verbose = 0 );
+  const List<Edge>& MST    () const { return mst_; }
+  double            Weight () const { return mstw_; }
+  Kruskal                  ( const G& g ) : g_(g),mst_(), mstw_(0.0),c_(0),pred_(),pq_(c_,pred_) {}
 
-  // implementing the algorithm
-  void Init(bool verbose = 0); // preps class variables for execution of algorithm
-  void Exec(bool verbose = 0); // runs algorithm
-
-  // extracting information
- // const fsu::List<Edge>& MST() const;
-  double                 Weight() const;
 private:
-  const Graph& g_;
-  PQ           pq_;
+  const G&   g_;    // undirected weighted graph
+  List<Edge> mst_;  // edges of MST  (calculated by Exec())
+  double     mstw_; // weight of MST (calculated by Exec())
+  Container       c_;    // "input" edge set
+  Predicate       pred_; // edge prioritizer
+  PQ              pq_;   // priority queue package operating on c_ with pred_
 };
 
 
-//implementations
-template < class G>
-void Kruskal<G>::Init(bool verbose)
-{
-    /*init queue in sorted order by increasing weight
-    //init partition p(|v|)
-    //init f as empty
-      if (verbose)
-  {
-    std::cout << "Init: Initial edges in PQ: " << pq_.Size() << '\n';
-    if (pq_.Size() < 20)
-    {
-      std::cout << "PQ.Dump():\n";
-      pq_.Dump(std::cout, '\n');
-      std::cout << '\n';
-    }   
-      }*/
-}
 
-template < class G>
-void Kruskal<G>::Exec(bool verbose)
-{
-    /*
-    [x,y] = q.Front();
-    q.Pop();
-    if(!p.Find(x,y))
-    {
-    F = F.Union{[x,y]};
-    p.Union(x,y);
-    }
-    output(v,F);
-    */
-    
-}
 #endif /* KRUSKAL_H */
